3a86bd856e58210e3cb39c30f7f426b1b2863a07
Isaac Kivuva Usugnio <isaacusungio@gmail.com>
2024-02-01 10:18:20 +0300

    home update

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,9 +1,18 @@
 import Hero from "../components/Hero/Hero"
+import ChooseUs from "../components/ChooseUs"
+import Trainers from "../components/Trainers"
+import Testimonials from "../components/Testimonials"
+import Gallery from "../components/Gallery"
+
 
 function Home(){
     return(
         <>
             <Hero/>
+            <ChooseUs/>
+            <Trainers/>
+            <Testimonials/>
+            <Gallery/>
         </>
     )
 }
