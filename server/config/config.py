6894fdf6e0441ac74db05f2212d508d4d68b672f
wkimunya <kimunya.wilson@gmail.com>
2024-02-09 10:09:58 +0300

    fix: FIx requirements

--- server/config/config.py
+++ server/config/config.py
@@ -2,7 +2,7 @@
 import os
 
 class Config:
-    SQLALCHEMY_DATABASE_URI = 'your_database_url_here'
+    SQLALCHEMY_DATABASE_URI = 'postgres://gym_mw66_user:nXgFYXB7MmEwh8D8xAXJRlcjL3xUtvph@dpg-cmv4tk2cn0vc73amn00g-a.oregon-postgres.render.com/gym_mw66'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     SECRET_KEY = 'your_secret_key_here'
     JWT_EXPIRATION_HOURS = 24  # Set the expiration time for JWT tokens
